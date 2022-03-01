d5739ebf4c7de10b816b8540cb1167335059731e
Max William <83377057+maxwsj@users.noreply.github.com>
2022-03-01 17:52:22 -0300

    Forkify-Project-Lessons

---
+++ Forkify-Project/src/js/config.js
@@ -0,0 +1,9 @@
+// We create a file to put important variables in one central place
+// Here we want variables that is important for defining data
+
+// In configurations like this, we use uppercase in our variables
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '72186187-a98e-43dc-a2ee-c488331a70c5';
+export const MODAL_CLOSE_SEC = 2500;
---
+++ Forkify-Project/src/js/views/searchView.js
@@ -0,0 +1,24 @@
+// This class do not render anything, all it does is listen for the click event on the button
+class SearchView {
+  _parentEl = document.querySelector('.search');
+
+  getQuery() {
+    const query = this._parentEl.querySelector('.search__field').value;
+    this._clearInput();
+    return query;
+  }
+
+  _clearInput() {
+    this._parentEl.querySelector('.search__field').value = '';
+  }
+
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+    });
+  }
+}
+
+// create a class and export it's instance and not the class itself
+export default new SearchView();
